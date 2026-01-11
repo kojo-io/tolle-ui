0ac73b3f07c2cc2281501e5ad2787f653e60829e
bernard.ossei <bernard.ossei@quixmocards.com>
2026-01-11 13:18:27 +0000

    update, added calendar docs

--- projects/docs/src/app/components/calendar-docs/calendar-docs.component.ts
+++ projects/docs/src/app/components/calendar-docs/calendar-docs.component.ts
@@ -2,7 +2,7 @@ import {Component, inject, OnInit} from '@angular/core';
 import {BaseService} from '../../shared/base.service';
 import {AnalyticsService} from '../../analytics.service';
 import {BaseEditorComponent} from '../../shared/base-editor/base-editor.component';
-import {NgIf} from '@angular/common';
+import { NgIf} from '@angular/common';
 import {SegmentedComponent} from '../../../../../tolle/src/lib/segment.component';
 import {FormsModule} from '@angular/forms';
 import {CalendarComponent} from '../../../../../tolle/src/lib/calendar.component';
@@ -23,6 +23,8 @@ import {CalendarComponent} from '../../../../../tolle/src/lib/calendar.component
 export class CalendarDocsComponent implements OnInit {
   baseService = inject(BaseService);
   analytics = inject(AnalyticsService);
+  date: any;
+  previewDate: any;
 
   ngOnInit(): void {
     this.analytics.init();
